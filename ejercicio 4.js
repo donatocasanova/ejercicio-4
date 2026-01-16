efc26571e26a449db713659902a5d49ec024fd83
donatocasanova <donatocasanovaa11@gmail.com>
2026-01-16 15:10:34 -0300

    Add files via upload

---
+++ ejercicio 4.js
@@ -0,0 +1,33 @@
+function crearDivs(n, m){
+    const container = document.createElement("div")
+    container.className = "container";
+
+        container.style.display = "grid";
+        container.style.gridTemplateColumns =  `repeat(${m}, 20px)`
+
+    for(let i = 0 ; i < n * m ; i++){
+        const div = document.createElement("div");
+
+        div.style.width = "20px"
+        div.style.height = "20px"
+        div.style.backgroundColor = colorAleatorio();
+
+
+        container.appendChild(div);
+    }
+    
+    
+    document.body.appendChild(container)
+    return container;
+
+}
+
+function colorAleatorio(){ 
+    const r = Math.floor(Math.random() *256);
+    const g = Math.floor(Math.random() *256);
+    const b = Math.floor(Math.random() *256);
+    return `rgb(${r} , ${g} , ${b})`
+}
+
+document.body.appendChild(crearDivs(3, 3));
+
